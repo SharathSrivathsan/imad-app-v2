d7724c05d99a36d1ccec4b96578a281992d57469
SharathSrivathsan <sharath.glazebrooke@gmail.com>
2017-02-12 16:54:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,13 @@ var element=document.getElementById("main-text");
 element.innerHTML="THE only SHARATH";
 
 var image=document.getElementById("img");
-var marginLeft=0;
+var marginLeft=0;                                 //the margin amount to the left of the image
 function moveRight(){
-    marginLeft=marginLeft+10;
-    image.style.marginLeft=marginLeft+'px';
+   
+    marginLeft=marginLeft+10;                   //incremented by 10 pixels     
+    image.style.marginLeft=marginLeft+'px';     //appending the px string to 10  ====>  '10px'
     
 }
 image.onclick=function(){
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,100);    //1 st argument tells directon and 2nd tells the time interval =====> 100 ms
 }
