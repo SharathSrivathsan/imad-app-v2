6e2d8ebba1a865ceedacc15acf84610d9a607495
SharathSrivathsan <sharath.glazebrooke@gmail.com>
2017-02-12 14:59:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ content:         `<p>
         </p>`    
   
 };
+function createtemplate(data){
+var title=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
 var htmltemplate=
    ` <html>
    <head>
@@ -47,13 +52,15 @@ var htmltemplate=
     </div>
     </div>
 </body>
-</html>`
+</html>`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(createtemplate(article));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
