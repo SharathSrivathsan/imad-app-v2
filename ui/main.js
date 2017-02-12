268d7618b653897b784bacf6e58529074f64213f
SharathSrivathsan <sharath.glazebrooke@gmail.com>
2017-02-12 16:50:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ var element=document.getElementById("main-text");
 element.innerHTML="THE only SHARATH";
 
 var image=document.getElementById("img");
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    image.style.marginLeft=marginLeft+'px';
+    
+}
 image.onclick=function(){
-    image.style.marginRight='100px';
+    var interval=setInterval(moveRight,100);
 }
