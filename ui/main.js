7ec3dfeeeac4684d566f9df047df371ec144058b
SharathSrivathsan <sharath.glazebrooke@gmail.com>
2017-02-12 19:30:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ button.onclick=function(){
    var request=new XMLHttpRequest();
    
    request.onreadystatechange=function(){
-   if(request.readystate===XMLhttprequest.DONE){
+   if(request.readystate===XMLHttpRequest.DONE){
        if(request.status===200){
            var counter=request.responseText;
            var span=document.getElementById('count');
