d3f81274325e31d5d8ca0b787a676866227fa230
SharathSrivathsan <sharath.glazebrooke@gmail.com>
2017-02-12 16:54:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,10 +8,10 @@ var image=document.getElementById("img");
 var marginLeft=0;                                 //the margin amount to the left of the image
 function moveRight(){
    
-    marginLeft=marginLeft+10;                   //incremented by 10 pixels     
-    image.style.marginLeft=marginLeft+'px';     //appending the px string to 10  ====>  '10px'
+    marginLeft=marginLeft+5;                   //incremented by 10 pixels     
+    image.style.marginLeft=marginLeft+'px';     //appending the px string to 10  ====>  '5px'
     
 }
 image.onclick=function(){
-    var interval=setInterval(moveRight,100);    //1 st argument tells directon and 2nd tells the time interval =====> 100 ms
+    var interval=setInterval(moveRight,50);    //1 st argument tells directon and 2nd tells the time interval =====> 50 ms
 }
