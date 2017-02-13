7385f0029d5484ea93ae59e620fe960d758b2523
SharathSrivathsan <sharath.glazebrooke@gmail.com>
2017-02-13 14:24:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.get('/', function (req, res) {
 
 
 
- app.get('/article-one.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one'));
 });   
 
 
