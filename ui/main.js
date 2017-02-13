05c5ceac51a18e7ca73d05c0c29042cce77c593b
SharathSrivathsan <sharath.glazebrooke@gmail.com>
2017-02-13 10:02:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,10 @@ var counter=0;
 button.onclick=function(){
   //create a request
    var request=new XMLHttpRequest();
-   
+  //capture the response and store it in a variable 
    request.onreadystatechange=function(){
    if(request.readystate===XMLHttpRequest.DONE){
-       if(request.status===200){
+       if(request.status===200){                               // 200: a successful request
            var counter=request.responseText;
            var span=document.getElementById('count');
            span.innerHTML=counter.toString();
@@ -15,7 +15,7 @@ button.onclick=function(){
    }
    };
    //make a request
-   request.open('GET','http://sharathsrivathsan.imad.hasura-app.io/counter',true);
+   request.open("GET","http://sharathsrivathsan.imad.hasura-app.io/counter",true);
    request.send(null);
    
     
